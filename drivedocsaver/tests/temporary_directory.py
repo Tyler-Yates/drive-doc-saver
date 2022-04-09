a5cf5ddafc446f9e78c48ece9ad3d1ca195f1be6
Tyler Yates <5478568+Tyler-Yates@users.noreply.github.com>
2022-04-08 20:09:35 -0500

    Better logging

--- drivedocsaver/tests/temporary_directory.py
+++ drivedocsaver/tests/temporary_directory.py
@@ -7,12 +7,15 @@ import tempfile
 class TemporaryDirectory(object):
     def __init__(self):
         if "RUNNER_TEMP" in os.environ:
-            temporary_dir_path = os.path.join(os.environ["RUNNER_TEMP"], str(random.randint(0, 9999999999999)))
+            runner_temp_dir_path = os.environ["RUNNER_TEMP"]
+            temp_dir_name = str(random.randint(0, 9999999999999))
+            temporary_dir_path = os.path.join(runner_temp_dir_path, temp_dir_name)
+            print(f"Using runner temporary directory {runner_temp_dir_path} and creating directory {temp_dir_name}")
             os.makedirs(temporary_dir_path)
             self.path = temporary_dir_path
         else:
             self.path = tempfile.mkdtemp()
-        print(f"Created temporary directory at {self.path}")
+            print(f"Created temporary directory at {self.path}")
 
     def __enter__(self):
         return self.path
